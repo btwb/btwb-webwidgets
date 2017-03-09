21b4a2898c8e97680739643ae3bd366fbd5ff462
Benjamin Yu <benjaminlyu@gmail.com>
2017-03-09 14:32:34 -0800

    Webwidgets 2.1 new HNF api URI

--- webwidgets/src/consts/uris.js
+++ webwidgets/src/consts/uris.js
@@ -1,9 +1,9 @@
 
-export const API_ROOT = 'https://api.beyondthewhiteboard.com/api/webwidgets/';
+export const API_ROOT = 'https://webwidgets.prod.btwb.com/webwidgets/';
 
-export const WODS_PATH = './gyms/wodsets';
+export const WODS_PATH = './wods';
 
-export const ACTIVITIES_PATH = './gyms/activities';
+export const ACTIVITIES_PATH = './activities';
 
-export const LEADERS_PATH = './gyms/workout_leaderboards';
+export const LEADERS_PATH = './leaderboards';
 
